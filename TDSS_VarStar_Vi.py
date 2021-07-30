348792c39e6572464abe106983b2c3059f8209e0
broulston <benjamin.roulston@protonmail.com>
2021-07-30 11:10:20 -0400

    Update TDSS_VarStar_Vi.py

--- TDSS_VarStar_Vi.py
+++ TDSS_VarStar_Vi.py
@@ -2,7 +2,13 @@
 # %autoreload 2
 
 import matplotlib
+import matplotlib.style as mplstyle
 matplotlib.use('TkAGG')
+mplstyle.use('fast')
+
+matplotlib.rcParams['path.simplify'] = True
+matplotlib.rcParams['path.simplify_threshold'] = 1.0
+matplotlib.rcParams['agg.path.chunksize'] = 10000
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
@@ -55,7 +61,7 @@ prop_out_dir, CSS_LC_plot_dir, ZTF_LC_plot_dir, Vi_plots_dir, datestr = vi.makeV
 nbins = 50
 TDSSprop = vi.TDSSprop(nbins)
 # ***********************************************
-latestFullVartoolsRun_filename = "completed_Vi_prop_2020-05-03.csv"
+latestFullVartoolsRun_filename = "completed_Vi_prop_2020-07-16.csv"
 latestFullVartoolsRun = vi.latestFullVartoolsRun(prop_out_dir + latestFullVartoolsRun_filename)
 # ***********************************************
 # TDSS_cssid_orginal = TDSSprop.TDSS_cssid
@@ -88,7 +94,21 @@ else:
     properties['ra'] = TDSSprop.data['ra']
     properties['dec'] = TDSSprop.data['dec']
 
+
+last_periodic_filenames = np.loadtxt(f"{prop_out_dir}periodic_objects_filenames_07-16-2020.txt", dtype="S")
+last_periodic_filenames = np.array([ii.decode() for ii in last_periodic_filenames])
 periodic_filenames = []
+all_filenames = []
+for prop_id, ROW in enumerate(tqdm.tqdm(TDSSprop.data)):
+        object_ra = ROW['ra']
+        object_dec = ROW['dec']
+        ra_string = '{:0>9.5f}'.format(object_ra)
+        dec_string = '{:0=+9.5f}'.format(object_dec)
+
+        this_Viplot_filename = f"{ra_string}{dec_string}_Vi.pdf"
+        all_filenames.append(this_Viplot_filename)
+
+all_filenames = np.array(all_filenames)
 # ***********************************************
 importlib.reload(vi)
 importlib.reload(LCtools)
@@ -102,13 +122,73 @@ runLS = True
 plotLCerr = True
 plt_resid = False
 plt_subLC = True
-plot_rejected = True
-checkalias = True
+plot_rejected = False
+checkHarmonic = True
 logProblimit = -10
 Nepochs_required = 10
 minP = 0.1
 maxP = 100.0
-nterms_LS = 1  # Currently astropy only allows calculation of the False Alarm probability (FAP) for neterms=1, so this can't be changed.
+# ***********************************************
+# ***********************************************
+# ***********************************************
+prop = Table.read("/Users/benjaminroulston/Dropbox/Research/TDSS/Variable_Stars/HARD_COPY_ORGINAL_DATA/PROGRAM_SAMPLE/2020-06-24/FINAL_FILES/TDSS_SES+PREV_DR16DR12griLT20_GAIADR2_Drake2014PerVar_VSX_CSSID_ZTFIDs_LCpointer_PyHammer_VI_ALLPROP_07-27-2020.fits")
+
+logProb_lim = -10
+CSS_periodic_index = (prop['CSS_logProb'] <= logProb_lim).data
+ZTFg_periodic_index = (prop['ZTF_g_logProb'] <= logProb_lim).data
+ZTFr_periodic_index = (prop['ZTF_r_logProb'] <= logProb_lim).data
+
+logProb_limit_array = np.stack((CSS_periodic_index, ZTFg_periodic_index, ZTFr_periodic_index), axis=1)
+logFAP_10 = logProb_limit_array.sum(axis=1).copy()
+
+logProb_lim = -5
+CSS_periodic_index = (prop['CSS_logProb'] <= logProb_lim).data
+ZTFg_periodic_index = (prop['ZTF_g_logProb'] <= logProb_lim).data
+ZTFr_periodic_index = (prop['ZTF_r_logProb'] <= logProb_lim).data
+
+logProb_limit_array = np.stack((CSS_periodic_index, ZTFg_periodic_index, ZTFr_periodic_index), axis=1)
+logFAP_5 = logProb_limit_array.sum(axis=1).copy()
+
+logFAP_10_unique, logFAP_10_unique_counts = np.unique(logFAP_10, return_counts=True)
+logFAP_5_unique, logFAP_5_unique_counts = np.unique(logFAP_5, return_counts=True)
+
+
+logFAP_10_1LC_index = np.where(logFAP_10 >= 1)[0]
+logFAP_5_2LC_index = np.where(logFAP_5 >= 2)[0]
+
+only_2LC_in_5 = []
+for ii in logFAP_5_2LC_index:
+    if ii in logFAP_10_1LC_index:
+        pass
+    else:
+        only_2LC_in_5.append(ii)
+
+only_2LC_in_5 = np.array(only_2LC_in_5)
+
+# ***********************************************
+# ***********************************************
+# ***********************************************
+
+CV_index = (np.char.strip(np.char.decode( prop['subclass'])) == "CV") | (np.char.strip(np.char.decode( prop['SUBCLASS_NOQSO'])) == "CV") | (np.char.strip(np.char.decode( prop['subClass_SDSSDR12'])) == "CV")
+large_Halpha_EQWe_index = (prop['EqW'] < 0).data
+
+move_ra = prop['ra'][large_Halpha_EQWe_index]
+move_dec = prop['dec'][large_Halpha_EQWe_index]
+moveDir = f"/Users/benjaminroulston/Desktop/large_Halpha_EQWe/"
+get_Vi_panels(move_ra, move_dec, '2020-07-16', copy=True, moveDir=moveDir)
+
+
+# ***********************************************
+# ***********************************************
+# ***********************************************
+is_C = np.array([(('dC' in ii) and ('+dC') not in ii) for ii in prop['PyHammerSpecType']])
+
+move_ra = prop['ra'][is_C]
+move_dec = prop['dec'][is_C]
+moveDir = f"/Users/benjaminroulston/Desktop/TDSS_VarStar_Cstar_ViP/"
+vi.get_Vi_panels(move_ra, move_dec, '2020-07-16', copy=True, moveDir=moveDir)
+
+
 # ***********************************************
 # ***********************************************
 # ***********************************************
@@ -127,18 +207,31 @@ with warnings.catch_warnings():
             properties[prop_id]['ViCompleted'] = 1
             continue
 
+        # if prop_id not in only_2LC_in_5:
+        #     properties[prop_id]['ViCompleted'] = 1
+        #     continue
+
         # if not ROW['PyHammerDiff'] & run_onlyPyHammerChanged:
         #     properties[prop_id]['ViCompleted'] = 1
         #     continue
         #
-        # prop_id = 12
+        # prop_id = 341
         # ROW = TDSSprop.data[prop_id]
-        start_time = time.time()
+
+        # periodic_index = 6
+        # prop_id = np.where(all_filenames == last_periodic_filenames[periodic_index])[0][0]
+        # ROW = TDSSprop.data[prop_id]
 
         object_ra = ROW['ra']
         object_dec = ROW['dec']
         ra_string = '{:0>9.5f}'.format(object_ra)
         dec_string = '{:0=+9.5f}'.format(object_dec)
+
+        this_Viplot_filename = f"{ra_string}{dec_string}_Vi.pdf"
+        if this_Viplot_filename not in last_periodic_filenames:
+            properties[prop_id]['ViCompleted'] = 1
+            continue
+
         mjd = ROW['mjd']
         plate = ROW['plate']
         fiberid = ROW['fiber']
@@ -155,10 +248,8 @@ with warnings.catch_warnings():
         ax4 = fig.add_subplot(gs[1, :])  # spectra with lines
         fig.suptitle(f'RA= {ra_string} DEC= {dec_string}', fontsize=16)
 
-        begin_first_plot = time.time()
-        CSS_prop, ZTF_g_prop, ZTF_r_prop, best_LC = vi.LC_analysis(ROW, TDSSprop, CSS_LC_dir, ZTF_g_LCs, ZTF_r_LCs, ax1, CSS_LC_plot_dir, ZTF_LC_plot_dir, Nepochs_required, minP=minP, maxP=maxP, nterms=nterms_LS, log10FAP=logProblimit, checkalias=checkalias, plt_subLC=plt_subLC, plot_rejected=plot_rejected)
+        CSS_prop, ZTF_g_prop, ZTF_r_prop, best_LC = vi.LC_analysis(ROW, TDSSprop, CSS_LC_dir, ZTF_g_LCs, ZTF_r_LCs, ax1, CSS_LC_plot_dir, ZTF_LC_plot_dir, Nepochs_required, minP=minP, maxP=maxP, log10FAP=logProblimit, checkHarmonic=checkHarmonic, plt_subLC=plt_subLC, plot_rejected=plot_rejected)
         all_LC_props = [CSS_prop, ZTF_g_prop, ZTF_r_prop]
-        end_first_plot = time.time()
 
         if best_LC == 'CSS':
             if (CSS_prop['logProb'] <= logProblimit):
@@ -175,36 +266,17 @@ with warnings.catch_warnings():
                 for key, value in LCprop.items():
                     properties[prop_id][prop_col_names_prefix[LC_ii] + key] = value
 
-        begin_second_plot = time.time()
         this_EqW = vi.plot_SDSSspec(ROW, TDSSprop, prop_id, spec_dir, ax4)
         properties[prop_id]['EqW'] = this_EqW
-        end_second_plot = time.time()
 
-        begin_third_plot = time.time()
-        vi.plot_middle(all_LC_props, best_LC, latestFullVartoolsRun, ax2)
-        end_third_plot = time.time()
+        vi.plot_middle(all_LC_props, best_LC, latestFullVartoolsRun, ax2, log10FAP=logProblimit)
 
-        begin_fourth_plot = time.time()
         vi.plot_CMD(TDSSprop, prop_id, ax3)
-        end_fourth_plot = time.time()
 
-        begin_save_plot = time.time()
         plt.savefig(f"{Vi_plots_dir}{ra_string}{dec_string}_Vi.pdf", dpi=600, bbox_inches='tight')
-        end_save_plot = time.time()
         # plt.show()
         plt.clf()
         plt.close()
-
-        print(start_time - begin_first_plot,
-              begin_first_plot - end_first_plot,
-              end_first_plot - begin_second_plot,
-              begin_second_plot - end_second_plot,
-              end_second_plot - begin_third_plot,
-              begin_third_plot - end_third_plot,
-              end_third_plot - begin_fourth_plot,
-              begin_fourth_plot - end_fourth_plot,
-              end_fourth_plot - begin_save_plot,
-              begin_save_plot - end_save_plot)
 
         properties[prop_id]['ViCompleted'] = 1
         if (prop_id % 100) == 0:
